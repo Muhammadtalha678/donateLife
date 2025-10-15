e4a3f1d7255b25c07e2e2a745690eedd6eaadf45
Muhammad Talha <muhammadtalha0110@gmail.com>
2025-10-15 04:20:40 +0000

    Donate life

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8405285481-a2878",
+  "appId": "1:772725567105:web:a25e7c6b9a339780f92780",
+  "apiKey": "AIzaSyAtpAVJJh0ktGjXS1Jtdru0KAWcpSq4Dcg",
+  "authDomain": "studio-8405285481-a2878.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "772725567105"
+};
